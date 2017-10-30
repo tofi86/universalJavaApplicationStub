65100ec4a6bd7735cf673274ea17fa0fec2f3031
Tobias Fischer <tobias.carl.fischer@gmail.com>
2017-10-30 17:05:37 +0100

    [refactoring] simplified function extractJavaMajorVersion()
    test results:
    
    ########################################################
    Testing function extractJavaMajorVersion()
    
    Tests with Java 1.6:
    [TEST OK] Extracted Java major version '6' for Java '1.6'
    [TEST OK] Extracted Java major version '6' for Java '1.6+'
    [TEST OK] Extracted Java major version '6' for Java '1.6.0'
    [TEST OK] Extracted Java major version '6' for Java '1.6.0_07'
    [TEST OK] Extracted Java major version '6' for Java '1.6.0_45'
    
    Tests with Java 1.7:
    [TEST OK] Extracted Java major version '7' for Java '1.7'
    [TEST OK] Extracted Java major version '7' for Java '1.7*'
    [TEST OK] Extracted Java major version '7' for Java '1.7.0'
    [TEST OK] Extracted Java major version '7' for Java '1.7.0_09'
    [TEST OK] Extracted Java major version '7' for Java '1.7.0_79'
    
    Tests with Java 1.8:
    [TEST OK] Extracted Java major version '8' for Java '1.8'
    [TEST OK] Extracted Java major version '8' for Java '1.8+'
    [TEST OK] Extracted Java major version '8' for Java '1.8.0'
    [TEST OK] Extracted Java major version '8' for Java '1.8.0_05'
    [TEST OK] Extracted Java major version '8' for Java '1.8.0_91'
    [TEST OK] Extracted Java major version '8' for Java '1.8.0_131'
    
    Tests with Java 9:
    [TEST OK] Extracted Java major version '9' for Java '9'
    [TEST OK] Extracted Java major version '9' for Java '9-ea'
    [TEST OK] Extracted Java major version '9' for Java '9.1*'
    [TEST OK] Extracted Java major version '9' for Java '9.0.1'
    [TEST OK] Extracted Java major version '9' for Java '9.0.1+'
    [TEST OK] Extracted Java major version '9' for Java '9.0.23'
    [TEST OK] Extracted Java major version '9' for Java '9.10.120'
    [TEST OK] Extracted Java major version '9' for Java '9.10.120+'
    [TEST OK] Extracted Java major version '9' for Java '9.100.120+'
    
    Tests with Java 10:
    [TEST OK] Extracted Java major version '10' for Java '10'
    [TEST OK] Extracted Java major version '10' for Java '10-ea'
    [TEST OK] Extracted Java major version '10' for Java '10.1+'
    [TEST OK] Extracted Java major version '10' for Java '10.0.1'
    [TEST OK] Extracted Java major version '10' for Java '10.0.1*'
    [TEST OK] Extracted Java major version '10' for Java '10.0.23'
    [TEST OK] Extracted Java major version '10' for Java '10.10.120'
    [TEST OK] Extracted Java major version '10' for Java '10.10.120+'
    [TEST OK] Extracted Java major version '10' for Java '10.100.120+'

--- test/java-version-tester.sh
+++ test/java-version-tester.sh
@@ -16,21 +16,10 @@ function extractJavaVersionString() {
 
 
 # helper function:
-# extract Java major version from java version string
-# - input '1.7.0_76' returns '7'
-# - input '1.8.0_121' returns '8'
-# - input '9-ea' returns '9'
-# - input '9.0.3' returns '9'
+# extract Java major version from a Java version string
 ##########################################################
 function extractJavaMajorVersion() {
-  java_ver=$1
-  # Java 6, 7, 8 start with '1.x'
-  if [ "${java_ver:0:2}" == "1." ] ; then
-    echo ${java_ver} | sed -E 's/1\.([0-8])[0-9_.]{2,6}/\1/g'
-  else
-    # Java 9+ start with 'x' using semver versioning
-    echo ${java_ver} | sed -E 's/([0-9]+)(-ea|(\.[0-9]+)*)/\1/g'
-  fi
+  echo $(echo "$1" | sed -E 's/^1\.//;s/^([0-9]+)(-ea|(\.[0-9_.]{1,7})?)[+*]?$/\1/')
 }
 
 
@@ -116,32 +105,48 @@ echo "########################################################"
 echo "Testing function extractJavaMajorVersion()"
 echo ""
 echo "Tests with Java 1.6:"
+testExtractMajor "1.6" "6"
+testExtractMajor "1.6+" "6"
 testExtractMajor "1.6.0" "6"
 testExtractMajor "1.6.0_07" "6"
 testExtractMajor "1.6.0_45" "6"
 echo ""
 echo "Tests with Java 1.7:"
+testExtractMajor "1.7" "7"
+testExtractMajor "1.7*" "7"
 testExtractMajor "1.7.0" "7"
 testExtractMajor "1.7.0_09" "7"
 testExtractMajor "1.7.0_79" "7"
 echo ""
 echo "Tests with Java 1.8:"
+testExtractMajor "1.8" "8"
+testExtractMajor "1.8+" "8"
 testExtractMajor "1.8.0" "8"
 testExtractMajor "1.8.0_05" "8"
 testExtractMajor "1.8.0_91" "8"
 testExtractMajor "1.8.0_131" "8"
 echo ""
 echo "Tests with Java 9:"
+testExtractMajor "9" "9"
 testExtractMajor "9-ea" "9"
+testExtractMajor "9.1*" "9"
 testExtractMajor "9.0.1" "9"
+testExtractMajor "9.0.1+" "9"
 testExtractMajor "9.0.23" "9"
 testExtractMajor "9.10.120" "9"
+testExtractMajor "9.10.120+" "9"
+testExtractMajor "9.100.120+" "9"
 echo ""
 echo "Tests with Java 10:"
+testExtractMajor "10" "10"
 testExtractMajor "10-ea" "10"
+testExtractMajor "10.1+" "10"
 testExtractMajor "10.0.1" "10"
+testExtractMajor "10.0.1*" "10"
 testExtractMajor "10.0.23" "10"
 testExtractMajor "10.10.120" "10"
+testExtractMajor "10.10.120+" "10"
+testExtractMajor "10.100.120+" "10"
 
 
 # test function:
