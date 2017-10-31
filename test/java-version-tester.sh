2a4f6e7de0da9cb47f16d715d003852c2510cbc4
Tobias Fischer <tobias.carl.fischer@gmail.com>
2017-10-31 20:14:13 +0100

    report invalid JVM requirement pattern in Plist key 'JVMVersion' as ERROR // resolves #50

--- test/java-version-tester.sh
+++ test/java-version-tester.sh
@@ -48,6 +48,51 @@ function get_comparable_java_version() {
 }
 
 
+# function 'is_valid_requirement_pattern()'
+#
+# check whether the Java requirement is a valid requirement pattern
+#
+# supported requirements are for example:
+# - 1.6       requires Java 6 (any update)      [1.6, 1.6.0_45, 1.6.0_88]
+# - 1.6*      requires Java 6 (any update)      [1.6, 1.6.0_45, 1.6.0_88]
+# - 1.6+      requires Java 6 or higher         [1.6, 1.6.0_45, 1.8, 9, etc.]
+# - 1.6.0     requires Java 6 (any update)      [1.6, 1.6.0_45, 1.6.0_88]
+# - 1.6.0_45  requires Java 6u45                [1.6.0_45]
+# - 1.6.0_45+ requires Java 6u45 or higher      [1.6.0_45, 1.6.0_88, 1.8, etc.]
+# - 9         requires Java 9 (any update)      [9.0.*, 9.1, 9.3, etc.]
+# - 9*        requires Java 9 (any update)      [9.0.*, 9.1, 9.3, etc.]
+# - 9+        requires Java 9 or higher         [9.0, 9.1, 10, etc.]
+# - 9.1       requires Java 9.1 (any update)    [9.1.*, 9.1.2, 9.1.13, etc.]
+# - 9.1*      requires Java 9.1 (any update)    [9.1.*, 9.1.2, 9.1.13, etc.]
+# - 9.1+      requires Java 9.1 or higher       [9.1, 9.2, 10, etc.]
+# - 9.1.3     requires Java 9.1.3               [9.1.3]
+# - 9.1.3*    requires Java 9.1.3 (any update)  [9.1.3]
+# - 9.1.3+    requires Java 9.1.3 or higher     [9.1.3, 9.1.4, 9.2.*, 10, etc.]
+# - 10-ea     requires Java 10 (early access release)
+#
+# unsupported requirement patterns are for example:
+# - 1.2, 1.3, 1.9       Java 2, 3 are not supported
+# - 1.9                 Java 9 introduced a new versioning scheme
+# - 6u45                known versioning syntax, but unsupported
+# - 9-ea*, 9-ea+        early access releases paired with */+
+# - 9., 9.*, 9.+        version ending with a .
+# - 9.1., 9.1.*, 9.1.+  version ending with a .
+# - 9.3.5.6             4 part version number is unsupported
+#
+# @param1  a Java requirement string ('1.8+')
+# @return  boolean exit code: 0 (is valid), 1 (is not valid)
+################################################################################
+function is_valid_requirement_pattern() {
+  local java_req=$1
+  # first test matches old Java versioning scheme (up to 1.8), second test matches new scheme (starting with 9)
+  if [[ ${java_req} =~ ^1\.[4-8](\.0)?(\.0_[0-9]+)?[*+]?$ ]] || [[ ${java_req} =~ ^(9|1[0-9])(-ea|[*+]|(\.[0-9]+){1,2}[*+]?)?$ ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+
 # function 'does_java_version_satisfy_requirement()'
 #
 # this function checks whether a given java version number
@@ -230,6 +275,106 @@ testComparable "10.1.3" "10001003"
 testComparable "10.12" "10012000"
 testComparable "10.10.23" "10010023"
 testComparable "10.10.113" "10010113"
+
+
+
+# test function:
+# tests the is_valid_requirement_pattern() function
+##########################################################
+function testValidReqPattern() {
+  local pattern=$1
+  local expected=$2
+  local actual=$(is_valid_requirement_pattern "$pattern" ; echo $?)
+  if [ "$expected" == "$actual" ] ; then
+    case $expected in
+      0)
+        echo "[TEST OK] [${expected}==${actual}] Requirement pattern '$pattern' is valid"
+        ;;
+      1)
+        echo "[TEST OK] [${expected}==${actual}] Requirement pattern '$pattern' is not valid"
+        ;;
+    esac
+  else
+    echo "[TEST FAILED] [${expected}!=${actual}] Requirement ${pattern} ; Expected: ${expected} ; Actual: ${actual}"
+  fi
+}
+
+
+echo ""
+echo ""
+echo "########################################################"
+echo "Testing function is_valid_requirement_pattern()"
+echo ""
+echo "Tests with old version scheme (valid requirements):"
+testValidReqPattern "1.4" "0"
+testValidReqPattern "1.5" "0"
+testValidReqPattern "1.6" "0"
+testValidReqPattern "1.6*" "0"
+testValidReqPattern "1.6+" "0"
+testValidReqPattern "1.6.0" "0"
+testValidReqPattern "1.6.0*" "0"
+testValidReqPattern "1.6.0+" "0"
+testValidReqPattern "1.6.0_45" "0"
+testValidReqPattern "1.6.0_45+" "0"
+testValidReqPattern "1.6.0_100" "0"
+testValidReqPattern "1.6.0_100+" "0"
+echo ""
+echo "Tests with old version scheme (invalid requirements):"
+testValidReqPattern "1.2" "1"
+testValidReqPattern "1.3" "1"
+testValidReqPattern "1.9" "1"
+testValidReqPattern "1.9*" "1"
+testValidReqPattern "1.9+" "1"
+testValidReqPattern "1.9.0_20" "1"
+testValidReqPattern "1.9.0_20*" "1"
+testValidReqPattern "1.9.0_20+" "1"
+testValidReqPattern "6u45" "1"
+echo ""
+echo "Tests with new version scheme (valid requirements):"
+testValidReqPattern "9" "0"
+testValidReqPattern "9*" "0"
+testValidReqPattern "9+" "0"
+testValidReqPattern "9-ea" "0"
+testValidReqPattern "9.1" "0"
+testValidReqPattern "9.1*" "0"
+testValidReqPattern "9.1+" "0"
+testValidReqPattern "9.1.3" "0"
+testValidReqPattern "9.1.3*" "0"
+testValidReqPattern "9.1.3+" "0"
+testValidReqPattern "9.0.13" "0"
+testValidReqPattern "9.11" "0"
+testValidReqPattern "9.11*" "0"
+testValidReqPattern "9.11+" "0"
+testValidReqPattern "9.10.23" "0"
+testValidReqPattern "9.10.101" "0"
+testValidReqPattern "10" "0"
+testValidReqPattern "10*" "0"
+testValidReqPattern "10-ea" "0"
+testValidReqPattern "10.1" "0"
+testValidReqPattern "10.1*" "0"
+testValidReqPattern "10.1+" "0"
+testValidReqPattern "10.0.1" "0"
+testValidReqPattern "10.0.1*" "0"
+testValidReqPattern "10.0.1+" "0"
+testValidReqPattern "10.0.13" "0"
+testValidReqPattern "10.1.3" "0"
+testValidReqPattern "10.12" "0"
+testValidReqPattern "10.10.23" "0"
+testValidReqPattern "10.10.113" "0"
+echo ""
+echo "Tests with new version scheme (invalid requirements):"
+testValidReqPattern "9-ea*" "1"
+testValidReqPattern "9-ea+" "1"
+testValidReqPattern "9." "1"
+testValidReqPattern "9.*" "1"
+testValidReqPattern "9.+" "1"
+testValidReqPattern "9.1." "1"
+testValidReqPattern "9.1.*" "1"
+testValidReqPattern "9.1.+" "1"
+testValidReqPattern "9.2.15." "1"
+testValidReqPattern "9.2.15.*" "1"
+testValidReqPattern "9.2.15.+" "1"
+testValidReqPattern "9.3.5.6" "1"
 
 
 # test function:
